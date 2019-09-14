44bd3ba0c7848a4e7a0e62f8befac5671ae7eb48
Shadow-of-N7 <boeck_daniel@web.de>
2019-09-14 12:37:30 +0200

    Fixed not returning program flow in deployment, other minor fixes

--- deploy_to_raspi.bat
+++ deploy_to_raspi.bat
@@ -4,14 +4,14 @@ set username=%2
 set password=%3
 
 REM Cleanup and recreate project folder
-%~dp0plink.exe %2@%1 -batch -pw %3 -m %~dp0commands_folder.txt
+%~dp0plink.exe %2@%1 -batch -pw %3 -m %~dp0deploy\commands_folder.txt
 REM Transfer files
 echo Starting file transfer...
 %~dp0pscp.exe -r -pw %3 %~dp0* %2@%1:/home/pi/SenseHatPatterns
 echo File transfer complete.
 REM Make scripts executable and execute
-%~dp0plink.exe %2@%1 -batch -pw %3 -m %~dp0commands_exec.txt
+%~dp0plink.exe %2@%1 -batch -pw %3 -m %~dp0deploy\commands_exec.txt
 timeout /T 2  > nul
-%~dp0plink.exe %2@%1 -batch -pw %3 -m %~dp0commands_start.txt
+%~dp0plink.exe %2@%1 -batch -pw %3 -m %~dp0deploy\commands_start.txt
 
 echo Deployment complete.
--- start_matrix.sh
+++ start_matrix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# nohup python3 /home/pi/SenseHatPatterns/control.py &
-python3 /home/pi/SenseHatPatterns/control.py /dev/null 2>&1 &
+nohup python3 /home/pi/SenseHatPatterns/control.py >>nohup.out 2>&1 &
+# python3 /home/pi/SenseHatPatterns/control.py /dev/null 2>&1 &
 
 
